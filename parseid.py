6d16eaa1ec273751b9cb06797c6d46172a1dc0e7
firebirdosint <141285729+firebirdosint@users.noreply.github.com>
2023-09-04 00:51:14 -0500

    Update parseid.py

--- parseid.py
+++ parseid.py
@@ -1,6 +1,19 @@
 import re
-with open("file.html","r") as f:
-	contents = f.read()
-	pattern = r'\d{21}'
-	matches = re.findall(pattern, contents)
-	print(matches)
+import argparse
+
+parser = argparse.ArgumentParser(description="Find 21-character strings of numbers in a file")
+
+parser.add_argument("-f", "--file", required=True, help="File name to extract ID from")
+
+args = parser.parse_args()
+
+try:
+    with open(args.file, "r") as f:
+        contents = f.read()
+except FileNotFoundError:
+    print(f"Error: File '{args.file}' not found.")
+    exit(1)
+
+pattern = r'\d{21}'
+matches = re.findall(pattern, contents)
+print(f"Found: {matches[0]}")
